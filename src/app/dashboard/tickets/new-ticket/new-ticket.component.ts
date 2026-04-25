3d940a7490b5f49457f964c0bb0f07c689133550
PMistry123 <pankti.pancholi88@gmail.com>
2026-04-25 15:41:19 -0400

    updated currentstatus with signal value, added viewchild and contentchild functionality as well as added template variables

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>
+  //private form = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  ngOnInit() {
+    console.log('On Init');
+    console.log(this.form?.nativeElement);
+  }
+
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
+    this.form?.nativeElement.reset();
+  }
+
+  ngAfterViewInit() {
+    console.log('After View Init');
+    console.log(this.form?.nativeElement);
+  }
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, inject, input, ElementRef, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, inject, input, ElementRef, ViewEncapsulation, ContentChild, contentChild, afterRender, afterNextRender } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -16,8 +16,24 @@ export class ControlComponent {
   label = input.required<string>();
   private el = inject(ElementRef);
 
+  constructor() {
+    afterRender(() => {
+      console.log("After Render");
+    })
+
+    afterNextRender(() => {
+      console.log("After Next Render");
+    })
+  }
+
+  //@ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
+
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input')
+
+
   onClick() {
     console.log('Clicked!');
     console.log(this.el);
+    console.log(this.control());
   }
 }
